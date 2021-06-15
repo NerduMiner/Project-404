28475472a55b801fc0d5788204de918dd3b8688a
intns <85960679+aioms@users.noreply.github.com>
2021-06-15 20:04:39 +0100

    Project 404
    Update README.md
    
    Update README.md
    
    Create Enemy Test script

---
+++ Assets/Scripts/Objects/Enemy/EnemyTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTest : MonoBehaviour
+{
+	[Header("Settings")]
+	[SerializeField] float _TimeTillShake = 2.5f;
+
+	float _ShakeTimer = 0;
+	EnemyDamageScript _Damage = null;
+
+	private void Awake()
+	{
+		_Damage = GetComponent<EnemyDamageScript>();
+	}
+
+	private void Update()
+	{
+		if (_Damage._AttachedPikmin.Count != 0)
+		{
+			_ShakeTimer += Time.deltaTime;
+
+			if (_ShakeTimer >= _TimeTillShake)
+			{
+				// Shake the pikmin off
+
+				int i = _Damage._AttachedPikmin.Count;
+				while (i > 0)
+				{
+					var pikmin = _Damage._AttachedPikmin[i - 1];
+					pikmin.ChangeState(PikminStates.Idle);
+					var rb = pikmin.GetComponent<Rigidbody>();
+					rb.isKinematic = false;
+					rb.AddForce(-pikmin.transform.forward * 10000);
+					i--;
+				}
+
+				_ShakeTimer = 0;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerUIController.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUIController : MonoBehaviour {
+  public Image _FadePanel = null;
+}
